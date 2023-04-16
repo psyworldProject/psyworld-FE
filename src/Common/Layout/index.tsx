66a20d8babf68c990e0327998b32c51273f62bc5
yousunzoo <dpfvmffjqm@gmail.com>
2023-04-17 02:16:47 +0900

    :sparkles: react-bootstrap-icons 설치 및 네비게이션 바 기본 세팅

--- src/Common/Layout/index.tsx
+++ src/Common/Layout/index.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { Link, Outlet } from "react-router-dom";
-import styled from "styled-components";
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+import styled from 'styled-components'
+import { Pencil, EmojiSmile, Image, House, JournalText } from 'react-bootstrap-icons'
 
 const NavbarContainer = styled.div`
   display: flex;
@@ -8,22 +9,36 @@ const NavbarContainer = styled.div`
   align-items: center;
   width: 300px;
   height: 50px;
-`;
+  svg {
+    width: 30px;
+    height: 30px;
+  }
+`
 
-const NavbarLink = styled(Link)``;
+const NavbarLink = styled(Link)``
 function Layout() {
   return (
     <>
       <Outlet />
       <NavbarContainer>
-        <NavbarLink to="/psylife">psylife</NavbarLink>
-        <NavbarLink to="/dairy">dairy</NavbarLink>
-        <NavbarLink to="/">minihome</NavbarLink>
-        <NavbarLink to="/photobook">photobook</NavbarLink>
-        <NavbarLink to="/guestbook">guestbook</NavbarLink>
+        <NavbarLink to='/psylife'>
+          <EmojiSmile />
+        </NavbarLink>
+        <NavbarLink to='/diary'>
+          <JournalText />
+        </NavbarLink>
+        <NavbarLink to='/'>
+          <House />
+        </NavbarLink>
+        <NavbarLink to='/photobook'>
+          <Image />
+        </NavbarLink>
+        <NavbarLink to='/guestbook'>
+          <Pencil />
+        </NavbarLink>
       </NavbarContainer>
     </>
-  );
+  )
 }
 
-export default Layout;
+export default Layout
